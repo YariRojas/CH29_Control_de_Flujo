08beea90961cd2b39e4e3f95275dc8c15984fc42
Yaritza Rojas <yari.rr26@gmail.com~>
2023-07-28 09:35:40 -0600

    Se agregaron 3 ejercicios de tarea

--- Parte 3/js/main.js
+++ Parte 3/js/main.js
@@ -1,5 +1,15 @@
 //Parte 3. Finalmente, cree una función que, a partir de un número de día del año (int, 0-365) y un número de hora (int, 0-23), devuelva verdadero si es horario comercial.
 
+function businessHours(dayNumber,hourNumber){
+    if ((dayNumber>=1) && (dayNumber<=5) && (hourNumber>=9) && (hourNumber<=18)){
+        return true;
+    }  else {
+        return false;
+    }  //if
+    }///businessHours
+
+
+
 function part3(yearDayNumber, hourNumber) {
     let day = getDayNumber(0, yearDayNumber);
     return businessHours(day, hourNumber);
